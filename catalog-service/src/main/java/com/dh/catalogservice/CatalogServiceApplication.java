7c13af9b7a617ba41f80de083e69025243b7c8fd
paulinaobertib <paulinaobertibusso@gmail.com>
2023-08-16 20:50:36 -0300

    yml catalog

--- catalog-service/src/main/java/com/dh/catalogservice/CatalogServiceApplication.java
+++ catalog-service/src/main/java/com/dh/catalogservice/CatalogServiceApplication.java
@@ -5,6 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 
 @SpringBootApplication
+@EnableDiscoveryClient
+@EnableFeignClients
 public class CatalogServiceApplication {
 
     public static void main(String[] args) {
