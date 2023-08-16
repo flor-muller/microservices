baaf6c3b7bc79506ec423236dc5932a405813813
flor-muller <mariaflorenciamuller@gmail.com>
2023-08-16 20:48:50 -0300

    configuracion eureka-server

--- movie-service/src/main/java/com/dh/movieservice/MovieServiceApplication.java
+++ movie-service/src/main/java/com/dh/movieservice/MovieServiceApplication.java
@@ -7,9 +7,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @EnableJpaRepositories
+@EnableDiscoveryClient
 public class MovieServiceApplication {
 
     public static void main(String[] args) {
