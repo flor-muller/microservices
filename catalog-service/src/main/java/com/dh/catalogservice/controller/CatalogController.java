ad1fc5e4cfe25c69556390cfbeaa192559c59d77
paulinaobertib <paulinaobertibusso@gmail.com>
2023-08-18 00:40:27 -0300

    terminado

--- catalog-service/src/main/java/com/dh/catalogservice/controller/CatalogController.java
+++ catalog-service/src/main/java/com/dh/catalogservice/controller/CatalogController.java
@@ -4,6 +4,8 @@ import com.dh.catalogservice.client.IMovieClient;
 import com.dh.catalogservice.model.Movie;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -17,11 +19,21 @@ public class CatalogController {
     @Autowired
     private IMovieClient IMovieClient;
 
+    @Autowired
+    private DiscoveryClient discoveryClient; // Para acceder a información de instancias de servicio
+
     @GetMapping("/{genre}")
     public ResponseEntity<List<Movie>> getMovieByGenre (@PathVariable String genre) {
         ResponseEntity<List<Movie>> response = IMovieClient.getMovieByGenre(genre);
-        System.out.println("puerto utilizado " + response.getHeaders().get("puerto"));
-        return IMovieClient.getMovieByGenre(genre);
+
+        // Obtiene información de la instancia de servicio actual
+        List<ServiceInstance> instances = discoveryClient.getInstances("movie-service");
+        if (!instances.isEmpty()) {
+            int port = instances.get(0).getPort();
+            System.out.println("Puerto del servicio Movie: " + port);
+        }
+
+        return response;
     }
 
     @PostMapping("/save")
