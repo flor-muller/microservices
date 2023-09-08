3c139cf911edc04823c0c33c6ea31cace1a37384
flor-muller <mariaflorenciamuller@gmail.com>
2023-09-08 12:57:20 -0300

    listener importacion

--- catalog-service/src/main/java/com/dh/catalogservice/queue/Listener.java
+++ catalog-service/src/main/java/com/dh/catalogservice/queue/Listener.java
@@ -3,7 +3,9 @@ package com.dh.catalogservice.queue;
 import com.dh.catalogservice.model.Movie;
 import com.dh.catalogservice.model.Serie;
 import com.dh.catalogservice.service.CatalogService;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Component;
 
 @Component
