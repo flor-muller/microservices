8a884eec9594aaf606a1ec07410b29ae896b5500
flor-muller <mariaflorenciamuller@gmail.com>
2023-09-08 10:35:48 -0300

    clase serie en catalogo

--- catalog-service/src/main/java/com/dh/catalogservice/model/Serie.java
+++ catalog-service/src/main/java/com/dh/catalogservice/model/Serie.java
@@ -1,10 +1,49 @@
 package com.dh.catalogservice.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.util.ArrayList;
 import java.util.List;
 
-public record Serie(String id, String name, String genre, List<Season> seasons)  {
-    public record Season(Integer seasonNumber, List<Chapter> chapters) {
-        public record Chapter(String name, Integer number, String urlStream) {
+@Document(collection = "Series")
+@AllArgsConstructor
+@NoArgsConstructor
+@Setter
+@Getter
+public class Serie {
+
+    @Id
+    private String id;
+    private String name;
+    private String genre;
+    private List<Season> seasons = new ArrayList<>();
+
+    @AllArgsConstructor
+    @NoArgsConstructor
+    @Setter
+    @Getter
+    public static class Season {
+
+        private Integer seasonNumber;
+        private List<Chapter> chapters = new ArrayList<>();
+
+        @AllArgsConstructor
+        @NoArgsConstructor
+        @Setter
+        @Getter
+        public static class Chapter {
+
+            private String name;
+            private Integer number;
+            private String urlStream;
+
+
         }
+
     }
 }
--- catalog-service/src/main/java/com/dh/catalogservice/repository/ISerieRepository.java
+++ catalog-service/src/main/java/com/dh/catalogservice/repository/ISerieRepository.java
@@ -1,8 +1,8 @@
 package com.dh.catalogservice.repository;
 
 import com.dh.catalogservice.model.Serie;
-import org.springframework.stereotype.Repository;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
 
 
 @Repository
